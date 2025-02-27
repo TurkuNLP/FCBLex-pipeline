b1c5b7c4facb9431814ca177dcac7ec549386d18
Tapio <tenojo@utu.fi>
2025-02-27 12:56:42 +0200

    Fixed postcleaning of texts after they've been processed in Word and before they get ran through Trankit

--- scripts/postclean_process_texts.sh
+++ scripts/postclean_process_texts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for f in Texts/*; do cat ${f} | egrep -v "^[:0-9:]+$" | perl -pe 's/\n/ /g' > tmp; mv tmp ${f}; done
+for f in Texts/*; do cat ${f} | egrep -v "^\s*[0-9]+\s*$" | perl -pe 's/[\r\n]+/ /g' > tmp; mv tmp ${f}; done
--- txt2conllu_pipeline.sh
+++ txt2conllu_pipeline.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+sh scripts/postclean_mac2unix.sh
+sleep 5
 sh scripts/postclean_process_texts.sh
+sleep 5
 python scripts/parseBooksTrankit.py
+sleep 5
 python scripts/trankitJson2Conllu.py
+echo "Done!"
